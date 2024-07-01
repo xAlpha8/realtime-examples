14de7d5bdee8a51443cb527436df9c6def8198f5
Janak <janak2@users.noreply.github.com>
2024-06-30 21:09:59 -0700

    fix 3d bot css

--- 3d_avatar_chatbot/frontend/src/App.jsx
+++ 3d_avatar_chatbot/frontend/src/App.jsx
@@ -8,8 +8,7 @@ import { RtAudio } from "@adaptai/realtime-react";
 import { ChatContext } from "./context";
 import "ldrs/square";
 import { ChatProvider } from "./context";
-import styles from "./index.module.css";
-import { isChrome, isSafari } from 'react-device-detect';
+import { isChrome, isSafari } from "react-device-detect";
 
 function RealtimeComponent({ config }) {
   const { isConnected, connection } = useRealtime(config);
@@ -39,7 +38,7 @@ function RealtimeComponent({ config }) {
 
   return (
     <>
-    {!isConnected && (
+      {!isConnected && (
         <div className="fixed top-0 left-0 inset-0 bg-black bg-opacity-50 z-[9999]">
           <div className="flex flex-col items-center h-screen">
             <l-square
@@ -53,7 +52,7 @@ function RealtimeComponent({ config }) {
             <div className="text-white text-3xl m-5">Connecting</div>
           </div>
         </div>
-    )}
+      )}
       <div>
         <RtAudio rtConnection={connection} />
       </div>
@@ -64,7 +63,8 @@ function RealtimeComponent({ config }) {
 function App() {
   const [config, setConfig] = useState(null);
   const configDefault = {
-    functionUrl: "https://infra.getadapt.ai/run/f5a918c27d35c60ac471efccef0d4d08",
+    functionUrl:
+      "https://infra.getadapt.ai/run/f5a918c27d35c60ac471efccef0d4d08",
     offerUrl: "",
     isDataEnabled: true,
     dataParameters: { ordered: true },
@@ -92,51 +92,63 @@ function App() {
 
   if (!isChrome && !isSafari) {
     return (
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', width: '100vw', backgroundColor: '#000', color: '#fff' }}>
-        <h1 style={{ fontSize: '3rem', textAlign: 'center' }}>This application only supports Chrome and Safari.</h1>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          width: "100vw",
+          backgroundColor: "#000",
+          color: "#fff",
+        }}
+      >
+        <h1 style={{ fontSize: "3rem", textAlign: "center" }}>
+          This application only supports Chrome and Safari.
+        </h1>
       </div>
     );
   }
-  
+
   return (
     <>
       <div className="self-start backdrop-blur-md bg-white bg-opacity-50 p-4 rounded-lg">
-        <>
+        <div className="space-y-4">
           <div>
-            <div>
-              <h2>Audio Options:</h2>
-              <select
-                value={audioInput}
-                onChange={(e) => setAudioInput(e.target.value)}
-              >
-                {audioOptions.map((option, index) => (
-                  <option key={index} value={option.value}>
-                    {option.label}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div>
-              <h2>Function URL:</h2>
-              <input
-                type="text"
-                value={functionUrl}
-                onChange={(e) => setFunctionUrl(e.target.value)}
-                placeholder="Enter Function URL"
-              />
-            </div>
-            <div>
-              <button
-                className={`bg-pink-500 hover:bg-pink-600 text-white p-4 px-10 font-semibold uppercase rounded-md opacity-30`}
-                onClick={dumpConfigAndRun}
-              >
-                Run
-              </button>
-            </div>
+            <h2 className="text-lg font-bold">Audio Options:</h2>
+            <select
+              className="w-full p-2 border border-gray-300 rounded-md"
+              value={audioInput}
+              onChange={(e) => setAudioInput(e.target.value)}
+            >
+              {audioOptions.map((option, index) => (
+                <option key={index} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </select>
           </div>
-          <div>{config && <RealtimeComponent config={config} />}</div>
-        </>
-      </div>
+          <div>
+            <h2 className="text-lg font-bold">Function URL:</h2>
+            <input
+              type="text"
+              className="w-full p-2 border border-gray-300 rounded-md"
+              value={functionUrl}
+              onChange={(e) => setFunctionUrl(e.target.value)}
+              placeholder="Enter Function URL"
+            />
+          </div>
+          <div>
+            <button
+              className="bg-pink-500 hover:bg-pink-600 text-white p-4 px-10 font-semibold uppercase rounded-md"
+              onClick={dumpConfigAndRun}
+            >
+              Run
+            </button>
+          </div>
+        </div>
+        <div>{config && <RealtimeComponent config={config} />}</div>
+      </div>{" "}
       <Loader />
       <UI />
       <Canvas shadows camera={{ position: [0, 0, 1], fov: 30 }}>
@@ -146,14 +158,14 @@ function App() {
   );
 }
 
-function Avatar() {  
+function Avatar() {
   return (
-    <div className={styles.body}>
+    <div className="h-screen w-screen m-0 bg-[#faaca8] bg-gradient-to-r from-[#faaca8] to-[#ddd6f3]">
       <ChatProvider>
         <App />
       </ChatProvider>
     </div>
-  )
+  );
 }
 
 export default Avatar;
--- 3d_avatar_chatbot/frontend/src/components/UI.jsx
+++ 3d_avatar_chatbot/frontend/src/components/UI.jsx
@@ -3,9 +3,8 @@ import { ChatContext } from "../context";
 
 export const UI = ({ hidden, ...props }) => {
   const input = useRef();
-  const {loading, cameraZoomed, setCameraZoomed } = useContext(ChatContext);
-  const sendMessage = () => {
-  };
+  const { loading, cameraZoomed, setCameraZoomed } = useContext(ChatContext);
+  const sendMessage = () => {};
 
   if (hidden) {
     return null;
@@ -50,31 +49,6 @@ export const UI = ({ hidden, ...props }) => {
                 />
               </svg>
             )}
-          </button>
-          <button
-            onClick={() => {
-              const body = document.querySelector("body");
-              if (body.classList.contains("greenScreen")) {
-                body.classList.remove("greenScreen");
-              } else {
-                body.classList.add("greenScreen");
-              }
-            }}
-            className="pointer-events-auto bg-pink-500 hover:bg-pink-600 text-white p-4 rounded-md"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
-              />
-            </svg>
           </button>
         </div>
         <div className="flex items-center gap-2 pointer-events-auto max-w-screen-sm w-full mx-auto">
