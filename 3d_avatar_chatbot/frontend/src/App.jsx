ffa00b7f08ac3abdc8fd5f259256b089616a1a02
Ankit Kumar <ankits.kvmdam@hotmail.com>
2024-08-16 06:16:21 +0900

    Add browser  check

--- 3d_avatar_chatbot/frontend/src/App.jsx
+++ 3d_avatar_chatbot/frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import { Loader } from "@react-three/drei";
 import React, { useState } from "react";
 import { useConfig } from "@adaptai/realtime-react";
+import { isSafari, isChrome } from "react-device-detect";
 import { DEFAULT_CONFIG } from "./constants/config";
 import { MainLayout } from "./MainLayout";
 import { InputForm } from "./Input";
+import { BrowserNotSupported } from "./BrowserNotSupported";
 
 /**
  * Main application component.
@@ -17,6 +19,10 @@ function App() {
   const { audioOptions } = options;
   const { setAudioInput, setFunctionUrl } = setters;
   const { audioInput, functionUrl } = values;
+
+  if (!isChrome && !isSafari) {
+    return <BrowserNotSupported />;
+  }
 
   return (
     <div className="h-screen w-screen m-0 bg-[#faaca8] bg-gradient-to-r from-[#faaca8] to-[#ddd6f3] overflow-hidden flex flex-col">
