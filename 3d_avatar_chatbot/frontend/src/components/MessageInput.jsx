2ca123ec13334307ec52141cd4b43c80155cc4e7
Janak <janak2@users.noreply.github.com>
2024-08-21 14:33:29 -0700

    Janak/eng 24 improve docs (#8)
    * simplify avatar structure
    
    * Update main.jsx
    
    * Refactor 3d chat bot examples (#7)
    
    * refactor
    
    * update lock
    
    * refactor and add doc strings
    
    * remove unused file
    
    * Refactor the structure
    
    * move facial expression logic to a new hook
    
    * extract most of the logic into a hook
    
    * removing scroll effect
    
    * add loader
    
    * Add browser  check
    
    ---------
    
    Co-authored-by: Ankit Kumar <ankits.kvmdam@hotmail.com>
    
    * reformat structure
    
    * add initial websocket code
    
    * remove audioanalyser
    
    * Update conversation.ts
    
    * Update conversation.ts
    
    * initial integration
    
    * minor fixes
    
    * Update conversation.ts
    
    * fix lip sync
    
    * Update chatbot.py
    
    * update connection logic
    
    * rename file
    
    * Update App.jsx
    
    * fix retryable connect
    
    * Update connection.ts
    
    ---------
    
    Co-authored-by: ankit-adapt <ankit.kumar@getadapt.ai>
    Co-authored-by: Ankit Kumar <ankits.kvmdam@hotmail.com>

---
+++ 3d_avatar_chatbot/frontend/src/components/MessageInput.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+/**
+ * Component for message input and send button.
+ *
+ * @param {Object} props - The component properties.
+ * @param {React.RefObject} props.inputRef - Ref object for the input field.
+ * @param {Function} props.sendMessage - Function to send the message.
+ * @returns {JSX.Element} The rendered input and button.
+ */
+export function MessageInput({ inputRef, sendMessage }) {
+  return (
+    <div className="fixed left-0 right-0 bottom-0 z-10 flex justify-between p-4 flex-col pointer-events-none">
+      <div className="flex items-center gap-2 pointer-events-auto max-w-screen-sm w-full mx-auto">
+        <input
+          className="w-full placeholder:text-gray-800 placeholder:italic p-4 rounded-md bg-opacity-50 bg-white backdrop-blur-md"
+          placeholder="Type a message..."
+          ref={inputRef}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              sendMessage();
+            }
+          }}
+        />
+        <button
+          onClick={sendMessage}
+          className="bg-pink-500 hover:bg-pink-600 text-white p-4 px-10 font-semibold uppercase rounded-md"
+        >
+          Send
+        </button>
+      </div>
+    </div>
+  );
+}
