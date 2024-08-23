1f7c52127fb57e0c09fdb7e37dcd7f1f510b35e3
Janak <janak2@users.noreply.github.com>
2024-08-22 17:08:51 -0700

    Update config.js

--- 3d_avatar_chatbot/frontend/src/constants/config.js
+++ 3d_avatar_chatbot/frontend/src/constants/config.js
@@ -19,7 +19,7 @@
  * @property {boolean} useStun - Indicates if a STUN server should be used for the connection.
  */
 export const DEFAULT_CONFIG = {
-  functionUrl: "https://infra.getadapt.ai/run/a39239eb9ff01b96ef2965a3a8695bac", // Function endpoint URL
+  functionUrl: "https://infra.getadapt.ai/run/2e0aa278b5471c709631b4498e73fb99", // Function endpoint URL
   offerUrl: "http://0.0.0.0:8080/", // URL to make offers for connections
   isDataEnabled: true, // Enable or disable data channels
   dataParameters: { ordered: true }, // Data channel parameters, e.g., ordered delivery
