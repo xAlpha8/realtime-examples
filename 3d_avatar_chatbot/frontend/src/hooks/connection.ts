537555540222c23133cf1d780d6a2df2740f95d9
Janak <janak2@users.noreply.github.com>
2024-09-02 19:34:37 -0700

    Update connection.ts

--- 3d_avatar_chatbot/frontend/src/hooks/connection.ts
+++ 3d_avatar_chatbot/frontend/src/hooks/connection.ts
@@ -235,16 +235,6 @@ export const useConversation = () => {
     };
     setSocket(socket);
 
-    // wait for socket to be ready
-    await new Promise((resolve) => {
-      const interval = setInterval(() => {
-        if (socket.readyState === WebSocket.OPEN) {
-          clearInterval(interval);
-          resolve(null);
-        }
-      }, 100);
-    });
-
     let audioStream;
     try {
       const trackConstraints: MediaTrackConstraints = {
@@ -286,7 +276,8 @@ export const useConversation = () => {
     socket!.send(
       stringify({
         type: "audio_metadata",
-        sampleRate: inputAudioMetadata.samplingRate,
+        inputSampleRate: inputAudioMetadata.samplingRate,
+        outputSampleRate: audioContext.sampleRate,
       })
     );
 
